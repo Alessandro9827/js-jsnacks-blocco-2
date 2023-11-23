88af4fb810f3a54bf4bd4a62426a54bab3dfe202
Alessandro9827 <ale.moramarco27@gmail.com>
2023-11-23 12:44:34 +0100

    snack-7

---
+++ snack-7/script.js
@@ -0,0 +1,19 @@
+//Cliccando su un pulsante, viene avviato un cronometro. 
+//Per fermare il cronometro, bisogna cliccare su un secondo pulsante
+
+const cronometro = document.querySelector('div.cronometro');
+const buttonStart = document.getElementById('btn-start');
+const buttonStop = document.getElementById('btn-stop');
+const output = document.querySelector('div.seconds h2');
+
+let clock;
+
+buttonStart.addEventListener('click', function(){
+    clock = setInterval(function() {
+        output.innerHTML = parseInt( output.innerText, 10) + 1
+    }, 1000);
+});
+
+buttonStop.addEventListener('click', function(){
+    clearInterval(clock)
+})
