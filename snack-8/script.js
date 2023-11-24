b80da57216b00dda36a024aace616e83eb8f5ee0
Alessandro9827 <ale.moramarco27@gmail.com>
2023-11-24 12:45:21 +0100

    snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,7 +1,32 @@
 //Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, 
 //peso e lunghezza. Calcola quanto pesano tutte le zucchine.
 
+const zucchina = {
+    varieta : '',
+    peso: 0,
+    lunghezza: 0,
+  };
+
+const zucchinaNera = Object.create(zucchina);
+zucchinaNera.varieta = 'Zucchina nera';
+zucchinaNera.peso = 0.5;
+zucchinaNera.lunghezza = 25; 
+
+const zucchinoFiorentino = Object.create(zucchina);
+zucchinoFiorentino.varieta = 'Zucchino fiorentino';
+zucchinoFiorentino.peso = 0.7;
+zucchinoFiorentino.lunghezza = 35; 
+
+const zucchinaNapoletana = Object.create(zucchina);
+zucchinaNapoletana.varieta = 'Zucchina napoletana';
+zucchinaNapoletana.peso = 0.9;
+zucchinaNapoletana.lunghezza = 20; 
+
+const zucchinaTonda = Object.create(zucchina);
+zucchinaTonda.varieta = 'Zucchine tonde';
+zucchinaTonda.peso = 0.4;
+zucchinaTonda.lunghezza = 12;
 
 
-
-
+const summZucchine = zucchinaNera.peso + zucchinoFiorentino.peso + zucchinaNapoletana.peso + zucchinaTonda.peso
+console.log(summZucchine);
